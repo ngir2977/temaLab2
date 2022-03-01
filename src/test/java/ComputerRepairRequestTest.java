a157b135fc0c5f8fe109b72ba5d319a5a4853e27
ngir2977 <ngir2977@scs.ubbcluj.ro>
2022-03-01 18:55:41 +0200

    Tema laborator 2

---
+++ src/test/java/ComputerRepairRequestTest.java
@@ -0,0 +1,22 @@
+import model.ComputerRepairRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ComputerRepairRequestTest {
+@Test
+@DisplayName("First Test")
+public void testExample(){
+    ComputerRepairRequest crr=new ComputerRepairRequest();
+    assertEquals("",crr.getOwnerName());
+    assertEquals("",crr.getOwnerAddress());
+}
+
+@Test
+@DisplayName("Second Test")
+public void testExample2()
+{
+    assertEquals(2,2,"Numerele sunt egale!");
+}
+}
